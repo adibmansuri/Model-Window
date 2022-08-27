d725ecbe9f3acd98e3298e5dace1f82f32bcadd6
Adib <mansuri.adib25@gmail.com>
2022-08-28 01:27:00 +0200

    add moda;

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ const btnClosemodel = document.querySelector('.close-modal');
 const btnopenmodel = document.querySelectorAll('.show-modal');
 console.log(btnopenmodel);
 for (let i = 0; i < btnopenmodel.length; i++)
-  console.log(btnopenmodel[i].textcontent);
+  console.log(
+    btnopenmodel[i].addEventListener('click', function () {
+      console.log('button click');
+      modal.classList.remove('hidden');
+      overlay.classList.remove('hidden');
+    })
+  );
