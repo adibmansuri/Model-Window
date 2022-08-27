f0a75944c7fbc1d4ebfc54c24a5b10a87a3b36c3
Adib <mansuri.adib25@gmail.com>
2022-08-27 19:09:06 +0200

    input Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosemodel = document.querySelector('.close-modal');
+const btnopenmodel = document.querySelectorAll('.show-modal');
+console.log(btnopenmodel);
