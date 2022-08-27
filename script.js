ca570df5d5d0ee1908ffde980ce3b059884576c7
Adib <mansuri.adib25@gmail.com>
2022-08-27 19:32:31 +0200

    add for loop

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const overlay = document.querySelector('.overlay');
 const btnClosemodel = document.querySelector('.close-modal');
 const btnopenmodel = document.querySelectorAll('.show-modal');
 console.log(btnopenmodel);
+for (let i = 0; i < btnopenmodel.length; i++)
+  console.log(btnopenmodel[i].textcontent);
